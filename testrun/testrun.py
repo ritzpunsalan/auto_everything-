0bb1f4d28e76773f8001f75ede7140932db4e75a
Ritz Punsalan <ritzpunsalan@gmail.com>
2020-06-14 01:01:41 -0700

    working on chapter 4

--- testrun/testrun.py
+++ testrun/testrun.py
@@ -5,30 +5,19 @@ E:\Desktop\neccessy books for coding\python books
 E:\Desktop\neccessy books for coding\python books\automate python\testrun
 
 """
-# chapter: 3
+# chapter: 4
 # This program says hello and asks for my name.
 
 # guessTheNumber.py
 # 74
 
-# This is a guess the number game.
-import random
-secretNumber = random.randint(1, 20)
-print('I am thinking of a number between 1 and 20.')
+# Getting Individual Values in a List with Indexes
 
-#Ask the player to guess 6 times.
-for guessesTaken in range(1, 7):
-    print('Take a guess.')
-    guess = int(input())
-
-    if guess < secretNumber:
-        print('Your guess is too low.')
-    elif guess > secretNumber:
-        print('Your guess is too high.')
-    else:
-        break  # this condition is the correct guess!
-    
-if guess == secretNumber:
-    print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
-else:
-    print('Nope.The number I was thinking of was ' + str(secretNumber))
+spam = ['cat', 'bat', 'rat', 'elephant']
+print(spam[0])
+print(spam[1])
+print(spam[2])
+print(spam[3])
+print(['cat', 'bat', 'rat', 'elephant'][3])
+print('Hello ' + spam[0])
+print('The ' + spam[1] + ' ate the ' + spam[0] + '.')
