3a70f94b07c6cf8d5b83eb93ced3eee0c752ef57
Ritz Punsalan <ritzpunsalan@gmail.com>
2020-06-15 21:00:20 -0700

    break on ch 4

--- lesson 4/ch4.py
+++ lesson 4/ch4.py
@@ -33,3 +33,157 @@ print('The ' + spam[1] + ' ate the ' + spam[0] + '.')
 works!! 
 
 """
+# 81
+
+"""
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+print(spam[10000])
+
+error works!!
+
+"""
+
+"""
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+print(spam[1])
+print(spam[1.0])
+
+error works!!
+
+"""
+
+"""
+
+spam = [['cat', 'bat'], [10, 20, 30, 40, 50]]
+print(spam[0])
+print(spam[0][1])
+print(spam[1][4])
+
+works!!
+
+"""
+# 82
+# Negative Indexes
+"""
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+print(spam[-1])
+print(spam[-3])
+print('The ' + spam[1] + ' is afraid of the ' + spam[3] + '.')
+
+works!!
+
+"""
+# Getting Sublists with Slices
+"""
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+print(spam[0:4])
+print(spam[1:3])
+print(spam[0:-1])
+
+works!!
+
+"""
+
+"""
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+print(spam[:2])
+print(spam[1:])
+
+works!!
+
+"""
+# 83
+# Getting a  List's length with len()
+"""
+
+spam = ['cat', 'dog', 'moose']
+print(len(spam))
+
+works!!
+
+"""
+# Changing Values in a List with Indexes
+"""
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+spam[1] = 'aardvark'
+print(spam)
+spam[2] = spam[1]
+print(spam)
+spam[-1] = 12345
+print(spam)
+
+works!!
+
+"""
+# List Concatenation and List Replication
+"""
+print([1, 2, 3] + ['A', 'B', 'c'])
+print(['X', 'Y', 'Z'] * 3)
+spam = [1, 2, 3]
+spam = spam + ['A', 'B', 'C']
+print(spam)
+
+works!!
+
+"""
+# 84
+# Removing Values from Lists with del Statements
+"""
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+del spam[2]
+print(spam)
+del spam[2]
+print(spam)
+
+works!!
+
+"""
+# allMyCats1.py
+# Working with Lists
+"""
+
+print('Enter the name of cat 1:')
+catName1 = input()
+print('Enter the name of cat 2:')
+catName2 = input()
+print('Enter the name of cat 3:')
+catName3 = input()
+print('Enter the name of cat 4:')
+catName4 = input()
+print('Enter the name of cat 5:')
+catName5 = input()
+print('Enter the name of cat 6:')
+catName6 = input()
+print('The cat names are:')
+print('catName1 + ' ' + catName2 +' ' + catName3 +' ' + catName4 + ' ' + catName5 + ' ' + catName6')
+
+works but an error in the end!!
+
+"""
+# allMyCats2.py
+# 85
+# Working with Lists
+"""
+
+catNames = []
+while True:
+    print('Enter the name of cat ' + str(len(catNames) + 1) +
+          ' (Or enter nothing to stop.):')
+    name = input()
+    if name == '':
+        break
+    catNames = catNames + [name]  # last concatenation
+print('The cat names are:')
+for name in catNames:
+    print('  ' + name)
+
+works!!
+
+"""
--- testrun/testrun.py
+++ testrun/testrun.py
@@ -8,16 +8,27 @@ E:\Desktop\neccessy books for coding\python books\automate python\testrun
 # chapter: 4
 # This program says hello and asks for my name.
 
-# guessTheNumber.py
-# 74
+# allMyCats1.py
+# 86
+# Working with Lists
 
-# Getting Individual Values in a List with Indexes
+for i in range(4):
+    print(i)
 
-spam = ['cat', 'bat', 'rat', 'elephant']
-print(spam[0])
-print(spam[1])
-print(spam[2])
-print(spam[3])
-print(['cat', 'bat', 'rat', 'elephant'][3])
-print('Hello ' + spam[0])
-print('The ' + spam[1] + ' ate the ' + spam[0] + '.')
+
+# spam = ['cat', 'bat', 'rat', 'elephant']
+# del spam[2]
+# print(spam)
+# del spam[2]
+# print(spam)
+# spam[-1] = 12345
+# print(spam)
+
+# print(spam[:])
+
+# print('The ' + spam[1] + ' is afraid of the ' + spam[3] + '.')
+# print(spam[2])
+# print(spam[3])
+# print(['cat', 'bat', 'rat', 'elephant'][3])
+# print('Hello ' + spam[0])
+# print('The ' + spam[1] + ' ate the ' + spam[0] + '.')
